8aa46587f12d72cc63be817de2a5bb62373fefd1
Ploruto <PhilippHaackCode@gmail.com>
2024-07-08 14:11:51 -0700

    Fix: Disable friendly-snippets

---
+++ lua/plugins/friendly-snippets.lua
@@ -0,0 +1,1 @@
+return { "friendly-snippets", enabled = false }
--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -6,6 +6,6 @@ return {
     -- load snippets paths
     require("luasnip.loaders.from_vscode").lazy_load({
       paths = { vim.fn.stdpath("config") .. "/snippets" },
-    })
+    }),
   end,
 }
