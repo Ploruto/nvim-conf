98385c701053ad492bd4701d5aa4fb113e01c3d3
Ploruto <PhilippHaackCode@gmail.com>
2024-07-09 04:24:10 -0700

    configure basic LuaSnip for .tex

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -4,8 +4,21 @@ return {
     -- include the default astronvim config that calls the setup call
     require("astronvim.plugins.configs.luasnip")(plugin, opts)
     -- load snippets paths
-    require("luasnip.loaders.from_vscode").lazy_load({
-      paths = { vim.fn.stdpath("config") .. "/snippets" },
-    }),
+    -- require("luasnip.loaders.from_vscode").lazy_load({
+    --   paths = { vim.fn.stdpath("config") .. "/snippets" },
+    -- })
+
+    -- Load all snippets from the nvim/LuaSnip directory at startup
+    require("luasnip.loaders.from_lua").lazy_load({paths = { vim.fn.stdpath("config") .. "/LuaSnip/"}})
+
+    require("luasnip").config.set_config({ -- Setting LuaSnip config
+
+      -- Enable autotriggered snippets
+      enable_autosnippets = true,
+
+      -- Use Tab (or some other key if you prefer) to trigger visual selection
+      store_selection_keys = "<Tab>",
+    })
+
   end,
 }
